a7cbae4ff63af0d76ff98fab1571c41232979c73
Nguyen Tri Huan <huannguyen2299@gmail.com>
2019-05-16 11:14:13 +0700

    function exercise

---
+++ function.js
@@ -0,0 +1,12 @@
+function showPopup() {
+  alert("I love JS");
+}
+showPopup();
+function showPopupWithLove(x) {
+  alert("I love "+x);
+}
+var x="to go cafe with you!";
+showPopupWithLove(x);
+function showLove() {
+  alert("I love C4T");
+}
