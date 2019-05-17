b6d06af9c8ea8e7fa831d110dc17478140cd890a
Nguyen Tri Huan <huannguyen2299@gmail.com>
2019-05-17 20:28:39 +0700

    final drill without more buttons and the optional :))

--- DOM.js
+++ DOM.js
@@ -1,17 +1,47 @@
-var song_container = document.getElementById("song_container");
-console.log(song_container);
-
+// output song_container
+var songContainer = document.getElementById("song_container");
+console.log(songContainer);
+//output = height and Width
 function boxInfo() {
   var el = document.getElementById("box");
   console.log("Height Box:" + el.clientHeight + "px");
   console.log("Width Box:" + el.clientWidth + "px");
 }
+//delete song
 var deleteButtons = document.getElementsByClassName("Delete");
 for (var i = 0; i < deleteButtons.length; i++) {
   var deleteButton = deleteButtons[i];
-  deleteButton.addEventListener('click',function(e){
+  deleteButton.addEventListener('click',(e) => {
     var btn = e.target;
     var div = btn.parentNode;
     div.remove();
   });
 }
+// In song_container, output = song, title and artist
+var songOutput = songContainer.getElementsByClassName("song");
+for (var i = 0; i < songOutput.length; i++) {
+  console.log(songOutput[i]);
+  var titleOutput = songOutput[i].querySelector(".title").textContent;
+  console.log(titleOutput);
+  var artistOutput = songOutput[i].querySelector(".artist").textContent;
+  console.log(artistOutput);
+}
+// Edit
+var editButtons = document.getElementsByClassName("Edit");
+for (var i = 0; i < editButtons.length; i++) {
+  var editButton = editButtons[i];
+    editButton.addEventListener('click', (e) => {
+      var btn = e.target;
+      var div = btn.parentNode;
+      var id = div.getAttribute("song_id");
+      console.log("song ID : " + id);
+    })
+}
+// More
+var moreButtons = document.getElementsByClassName("More");
+for (var i = 0; i < moreButtons.length; i++) {
+  var moreButton = moreButtons[i];
+    moreButton.addEventListener('click', (e) => {
+      var btn = e.target;
+      var div = btn.parentNode;
+      
