87d8c915af2c9e844e4d276da8b8d1f83a248544
Nguyen Tri Huan <huannguyen2299@gmail.com>
2019-05-17 11:17:25 +0700

    delete song

--- DOM.js
+++ DOM.js
@@ -1,7 +1,17 @@
-var song_container=document.getElementById("song_container");
+var song_container = document.getElementById("song_container");
 console.log(song_container);
+
 function boxInfo() {
   var el = document.getElementById("box");
-  console.log("Height Box:" + el.clientHeight + "px<br>");
+  console.log("Height Box:" + el.clientHeight + "px");
   console.log("Width Box:" + el.clientWidth + "px");
+}
+var deleteButtons = document.getElementsByClassName("Delete");
+for (var i = 0; i < deleteButtons.length; i++) {
+  var deleteButton = deleteButtons[i];
+  deleteButton.addEventListener('click',function(e){
+    var btn = e.target;
+    var div = btn.parentNode;
+    div.remove();
+  });
 }
