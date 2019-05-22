48d0a4c47d29b7137ec5c042400ec8fdb5f79aac
Nguyen Tri Huan <huannguyen2299@gmail.com>
2019-05-23 00:09:54 +0700

    final drill

--- DOM.js
+++ DOM.js
@@ -8,10 +8,10 @@ function boxInfo() {
   console.log("Width Box:" + el.clientWidth + "px");
 }
 //delete song
-var deleteButtons = document.getElementsByClassName("Delete");
+var deleteButtons = document.getElementsByClassName("delete");
 for (var i = 0; i < deleteButtons.length; i++) {
   var deleteButton = deleteButtons[i];
-  deleteButton.addEventListener('click',(e) => {
+  deleteButton.addEventListener('click', (e) => {
     var btn = e.target;
     var div = btn.parentNode;
     div.remove();
@@ -26,22 +26,35 @@ for (var i = 0; i < songOutput.length; i++) {
   var artistOutput = songOutput[i].querySelector(".artist").textContent;
   console.log(artistOutput);
 }
-// Edit
-var editButtons = document.getElementsByClassName("Edit");
+// dit
+var editButtons = document.getElementsByClassName("edit");
 for (var i = 0; i < editButtons.length; i++) {
-  var editButton = editButtons[i];
-    editButton.addEventListener('click', (e) => {
-      var btn = e.target;
-      var div = btn.parentNode;
-      var id = div.getAttribute("song_id");
-      console.log("song ID : " + id);
-    })
+  var editButton = editButtediti];
+  editButton.addEventListener('click', (e) => {
+    var btn = e.target;
+    var div = btn.parentNode;
+    var id = div.getAttribute("song_id");
+    console.log("song ID : " + id);
+  })
 }
 // More
-var moreButtons = document.getElementsByClassName("More");
+var moreButtons = document.getElementsByClassName("more");
 for (var i = 0; i < moreButtons.length; i++) {
   var moreButton = moreButtons[i];
-    moreButton.addEventListener('click', (e) => {
-      var btn = e.target;
-      var div = btn.parentNode;
-      
+  moreButton.addEventListener('click', (e) => {
+    var btn = e.target;
+    var div = btn.parentNode;
+    console.log("Song name: " + div.getElementsByClassName("title")[0].innerHTML + "\nArtist: " + div.getElementsByClassName("artist")[0].innerHTML + "\nSong ID: " + div.getAttribute("song_id"));
+  })
+}
+//Add
+function createButtons(class_name, text) {
+  var btn = document.createElement("button");
+  btn.className = class_name;
+  btn.type = "button";
+  btn.innerHTML = text;
+  return btn;
+}
+function addButtons() {
+  
+}
